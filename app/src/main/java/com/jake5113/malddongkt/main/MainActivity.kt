c7f9dbb2e559724f1815632a7e595bd1bf41aa67
jake5113 <jake5113@gmail.com>
2023-04-02 23:22:40 +0900

    Mani - 세로고정 MainActivity - bottom navi 탭할때 - appbar 변경

--- app/src/main/java/com/jake5113/malddongkt/main/MainActivity.kt
+++ app/src/main/java/com/jake5113/malddongkt/main/MainActivity.kt
@@ -1,14 +1,10 @@
 package com.jake5113.malddongkt.main
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
-import android.widget.Toast
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationBarView
+import android.view.View
+import androidx.core.view.get
 import com.jake5113.malddongkt.R
-import com.jake5113.malddongkt.databinding.ActivityLoginBinding
 import com.jake5113.malddongkt.databinding.ActivityMainBinding
 import com.jake5113.malddongkt.main.map.NaverMapFragment
 import com.jake5113.malddongkt.main.toilet.ToiletListFragment
@@ -19,15 +15,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        supportFragmentManager.beginTransaction().add(R.id.fragment_container_view, ToiletListFragment()).commit()
 
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container_view, ToiletListFragment()).commit()
 
         binding.bottomNavigation.setOnItemSelectedListener {
-            when (it.itemId){
-                R.id.item_list -> supportFragmentManager.beginTransaction().add(R.id.fragment_container_view, ToiletListFragment()).commit()
-                R.id.item_map -> supportFragmentManager.beginTransaction().add(R.id.fragment_container_view, NaverMapFragment()).commit()
-                R.id.item_favorite -> supportFragmentManager.beginTransaction().add(R.id.fragment_container_view, NaverMapFragment()).commit()
-                else -> false
+            when (it.itemId) {
+                R.id.item_list -> {
+                    binding.appbarMap.visibility = View.GONE
+                    binding.appbarList.visibility = View.VISIBLE
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container_view, ToiletListFragment()).commit()
+                }
+
+                R.id.item_map -> {
+                    binding.appbarMap.visibility = View.VISIBLE
+                    binding.appbarList.visibility = View.GONE
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container_view, NaverMapFragment()).commit()
+                }
+
+                R.id.item_favorite -> {
+                    binding.appbarMap.visibility = View.GONE
+                    binding.appbarList.visibility = View.VISIBLE
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container_view, NaverMapFragment()).commit()
+                }
             }
             true
         }
---
+++ app/src/main/java/com/jake5113/malddongkt/main/toilet/ToiletRecyclerAdapter.kt
@@ -0,0 +1,24 @@
+package com.jake5113.malddongkt.main.toilet
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+class ToiletRecyclerAdapter : Adapter<ToiletRecyclerAdapter.VH>() {
+    inner class VH(itemView: View) : ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
